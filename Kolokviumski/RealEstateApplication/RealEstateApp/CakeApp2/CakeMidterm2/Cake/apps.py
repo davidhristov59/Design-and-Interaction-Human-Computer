8544db4262e1838ae835ca689b635523da9de6fd
David Hristov <113634059+davidhristov59@users.noreply.github.com>
2025-07-02 01:12:08 +0200

    Delete Kolokviumski/RealEstateApplication/RealEstateApp/CakeApp2 directory

--- Kolokviumski/RealEstateApplication/RealEstateApp/CakeApp2/CakeMidterm2/Cake/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CakeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Cake'
-
-    def ready(self):
-        from . import signals
